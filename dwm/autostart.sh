b12259e8d4ed942b19b8e6579b066116f4b8d3b7
MTTbrandUser <77514703+MTTbrandUser@users.noreply.github.com>
2021-09-15 20:38:30 +0200

    Add files via upload

---
+++ dwm/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lxsession &
+picom &
+nitrogen --restore &
+/usr/bin/emacs --daemon &
+nm-applet &
+dunst &
+lxqt-policykit-agent &
+xfce4-power-manager &
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off &
+#flameshot &
+/home/$USER/.config/dwm-bar/dwm_bar.sh &
