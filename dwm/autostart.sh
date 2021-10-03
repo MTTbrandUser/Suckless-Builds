58534fb75fdc086acd1c6c8cfd3daf10c7a6d9ce
MTTbrandUser <77514703+MTTbrandUser@users.noreply.github.com>
2021-10-03 14:04:45 +0200

    Add files via upload

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,8 +6,9 @@ nitrogen --restore &
 /usr/bin/emacs --daemon &
 nm-applet &
 dunst &
-lxqt-policykit-agent &
+#lxqt-policykit-agent &
+polkit-gnome-authentication-agent-1 &
 xfce4-power-manager &
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off &
 #flameshot &
-/home/$USER/.config/dwm-bar/dwm_bar.sh &
+slstatus &
--- dwm/config.h
+++ dwm/config.h
@@ -2,12 +2,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Mononoki Nerd Font:size=10" };
-static const char dmenufont[]       = "Mononoki Nerd Font:size=10";
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
+static const char *fonts[]          = { "roboto:size=10" };
+static const char dmenufont[]       = "roboto:size=10";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#44475a";
 static const char col_gray3[]       = "#f8f8f2";
@@ -28,7 +30,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
@@ -43,13 +45,13 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
- /* key definitions */
+/* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-        { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-        { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-        { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-        { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -58,16 +60,16 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty" };
-static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *thunarcmd[] = { "thunar", NULL };
+static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,			XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask, 		XK_r, 	   spawn,          {.v = roficmd } },
-	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask, 		XK_Return, 	   spawn,          {.v = thunarcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,		                XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,     		XK_Return, 	   spawn,          {.v = thunarcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask, 		XK_r, 	   spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
