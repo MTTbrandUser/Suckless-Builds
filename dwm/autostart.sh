3681791932ef6302f2b3b072097e6c6ff1a22578
MTTbrandUser <77514703+MTTbrandUser@users.noreply.github.com>
2021-11-15 21:30:43 +0100

    Add files via upload

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -12,3 +12,4 @@ xfce4-power-manager &
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off &
 #flameshot &
 slstatus &
+setxkbmap pl &
--- dwm/config.h
+++ dwm/config.h
@@ -8,13 +8,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 5;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Iosevka:size=10" };
-static const char dmenufont[]       = "Iosevka:size=10";
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#44475a";
-static const char col_gray3[]       = "#f8f8f2";
-static const char col_gray4[]       = "#f8f8f2";
-static const char col_cyan[]        = "#bd93f9";
+static const char *fonts[]          = { "JetBrains Mono:size=10" };
+static const char dmenufont[]       = "JetBrains Mono:size=10";
+static const char col_gray1[]       = "#141d22";
+static const char col_gray2[]       = "#2f2e33";
+static const char col_gray3[]       = "#f9e7fb";
+static const char col_gray4[]       = "#f9e7fb";
+static const char col_cyan[]        = "#29b1a0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty" };
-static const char *thunarcmd[] = { "nemo", NULL };
+static const char *thunarcmd[] = { "thunar", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 
 static Key keys[] = {
