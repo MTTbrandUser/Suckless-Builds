f52ebe9b7a4d268fd1fe69902d3f1e8f6f468419
MTTbrandUser <77514703+MTTbrandUser@users.noreply.github.com>
2021-11-25 18:44:04 +0100

    Update autostart.sh

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -3,11 +3,8 @@
 lxsession &
 picom &
 nitrogen --restore &
-/usr/bin/emacs --daemon &
-nm-applet &
 dunst &
-#lxqt-policykit-agent &
-polkit-gnome-authentication-agent-1 &
+lxqt-policykit-agent &
 xfce4-power-manager &
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off &
 #flameshot &
